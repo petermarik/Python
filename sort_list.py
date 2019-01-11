a8a071ec400e50878e51946efc8b6d09447f02ba
petermarik <39341317+petermarik@users.noreply.github.com>
2019-01-11 15:25:04 -0200

    Add files via upload

---
+++ sort_list.py
@@ -0,0 +1,11 @@
+def sort_list_number():
+    numlist=[]
+    x=int(input("1st number: "))
+    numlist.append(x)
+    y=int(input("2nd number: "))
+    numlist.append(y)
+    z=int(input("3rd number: "))
+    numlist.append(z)
+    print(sorted(numlist))
+   
+sort_list_number()
